e9e5602161862d8d2dfc53384fb85ac91ca84fec
Petr Noga <n.skypo@gmail.com>
2017-03-02 13:02:39 +0100

    Add files via upload

---
+++ cviceni4/cviceni4/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cviceni4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tree = new Stack<int>();
+            tree.Push(5);
+            tree.Push(6);
+
+            Stack<string> tree1 = new Stack<string>();
+            tree1.Push("aaa");
+            tree1.Push("bbb");
+
+            Console.WriteLine(tree1.Pop());
+            Console.WriteLine(tree1.Pop());
+            Console.WriteLine(tree.Pop());
+            Console.WriteLine(tree.Pop());
+
+
+        }
+    }
+}
---
+++ cviceni4/cviceni4/Stack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviko5
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Stack<T>
+    {
+        private int pointer = -1;
+        private T[] stack;
+
+        public Stack()
+        {
+            this.stack = new T[15];
+        }
+
+        public void Clear()
+        {
+            this.pointer = -1;
+        }
+
+        public bool isEmpty()
+        {
+            return this.pointer == -1;
+        }
+
+        public bool isFull()
+        {
+            return this.pointer == this.stack.Length - 1;
+        }
+
+        public T Pop()
+        {
+            if (this.pointer > -1 && this.pointer <= 15)
+            {
+                T tmp = this.stack[this.pointer];
+                this.pointer--;
+                return tmp;
+            }
+            else
+            {
+                throw new ApplicationException();
+            }
+        }
+
+        public void Push(T number)
+        {
+            if (this.pointer <= 15)
+            {
+                this.pointer++;
+                this.stack[this.pointer] = number;
+            }
+            else
+            {
+                throw new ApplicationException();
+            }
+        }
+
+        public T Top()
+        {
+            return this.stack[this.pointer];
+        }
+    }
+
+
+}
