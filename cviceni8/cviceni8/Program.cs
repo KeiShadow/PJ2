d3d9cd5d039111c63016e2d03cef15e91eb3e3b3
Petr Noga <n.skypo@gmail.com>
2017-03-30 13:41:34 +0200

    Pridana trida Num

--- cviceni8/cviceni8/Program.cs
+++ cviceni8/cviceni8/Program.cs
@@ -10,24 +10,54 @@ namespace cviceni8
     {
         static void Main(string[] args)
         {
-            double[] numbers = new double[10];
-            for (int i = 0; i < 10; i++) {
-                numbers[i] = i;
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            string text = "Ahoj světe jak se máš?";
+            string[] words = text.Split(' ');
+
+            dict["Ahoj"] = "Hello";
+            dict["světe"] = "world";
+            dict["jak"] = "how";
+            dict["se"] = "are";
+            dict["máš?"] = "you?";
+
+            foreach (string word in words) {
+                Console.Write(dict[word]);
+                Console.Write(" ");
             }
 
-            Array.Sort(numbers, new Num());
 
-            foreach (int num in numbers )
-            {
-                Console.WriteLine(num);
-            }
+
+
             /*
-            Calendar cal = new Calendar();
-            Calendar2 cal2 = new Calendar2();
+                        Dictionary<string, bool> dict = new Dictionary<string, bool>();
 
-            foreach (DateTime date in cal2) {
-                Console.WriteLine(date);
-            }*/
+
+                        dict["ahoj"] = true;
+                        dict["tome"] = false;
+
+                        if (dict.ContainsKey("tome") && dict["tome"]) {
+                            Console.WriteLine("jop");
+                        }
+
+
+                       double[] numbers = new double[10];
+                       for (int i = 0; i < 10; i++) {
+                           numbers[i] = i;
+                       }
+
+                       Array.Sort(numbers, new Num());
+
+                       foreach (int num in numbers )
+                       {
+                           Console.WriteLine(num);
+                       }
+
+                       Calendar cal = new Calendar();
+                       Calendar2 cal2 = new Calendar2();
+
+                       foreach (DateTime date in cal2) {
+                           Console.WriteLine(date);
+                       }*/
         }
     }
 }
